1c1ee4e99b53c40fce625f16b1b0a9aa6d2d4e48
srikant mehta <jmdsr2@gmail.com>
2023-09-15 18:00:52 +0530

    4th commit

--- app/new-task/page.jsx
+++ app/new-task/page.jsx
@@ -14,7 +14,7 @@ export default function AddTopic() {
     if (!title || !description) { alert("Title and description are required."); return; }
 
     try {
-      const res = await fetch("http://localhost:3000/api/topics", {
+      const res = await fetch("/api/topics", {
         method: "POST",
         headers: { "Content-type": "application/json" },
         body: JSON.stringify({ title, description }),
