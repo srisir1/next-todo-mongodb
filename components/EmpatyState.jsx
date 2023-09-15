be38d8c8e61c0d7a0aaf66e6f0372abf45b645c5
srikant mehta <jmdsr2@gmail.com>
2023-09-15 15:31:23 +0530

    1st commit

---
+++ components/EmpatyState.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmpatyState = () => {
+  return (
+    <div className='flex justify-center items-center h-[20vh]'>
+      <p className='text-2xl text-red-400 font-bold'>No Task Found</p>
+    </div>
+  )
+}
+
+export default EmpatyState;
---
+++ libs/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectMongoDB = async () => {
+  try {
+    if (mongoose.connection.readyState > 0) return;
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB.");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectMongoDB;
